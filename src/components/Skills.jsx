7cbc18766952a0e479bfa70fb9c67dd59360be0a
chathunipr <chathuninperera@gmail.com>
2024-07-02 23:24:00 +0530

    Updating and fixing errors in the Skills Page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
+const skills = [
+  { name: "Java", icon: "assets/javas.png" },
+  { name: "HTML", icon: "assets/html.png" },
+  { name: "CSS", icon: "assets/css.png" },
+  { name: "React", icon: "assets/react.png" },
+  { name: "Flutter", icon: "assets/flutter.png" },
+  { name: "Node js", icon: "assets/nodejs.png" },
+  { name: "Github", icon: "assets/github.png" },
+  { name: "Mongo DB", icon: "assets/mongo.png" },
+  { name: "Jira", icon: "assets/jiras.png" },
+  { name: "Postman", icon: "assets/postman.png" },
+];
+
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f]  text-gray-300">
+    <div name="skills" className="w-full bg-[#0a192f] text-gray-300 min-h-[calc(100vh-64px)] md:pt-[calc(100px+1rem)]">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-start w-full h-full">
+        <div
+          data-aos="fade-up"
+          data-aos-delay="100"
+          data-aos-duration="300"
+
+        >
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
             Skills
           </p>
@@ -13,82 +31,19 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/javas.png"
-              alt="Java icon"
-            />
-            <p className="my-4">Java</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/html.png"
-              alt="HTML icon"
-            />
-            <p className="my-4">HTML</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img className="w-20 mx-auto" src="assets/css.png" alt="CSS icon" />
-            <p className="my-4">CSS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/react.png"
-              alt="React icon"
-            />
-            <p className="my-4">React</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/flutter.png"
-              alt="Flutter icon"
-            />
-            <p className="my-4">Flutter</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/nodejs.png"
-              alt="Nodejs icon"
-            />
-            <p className="my-4">Node js</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/github.png"
-              alt="Github icon"
-            />
-            <p className="my-4">Github</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/mongo.png"
-              alt="Mongodb icon"
-            />
-            <p className="my-4">Mongo DB</p>
-          </div>{" "}
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/jiras.png"
-              alt="Jira icon"
-            />
-            <p className="my-4">Jira</p>
-          </div>{" "}
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg">
-            <img
-              className="w-20 mx-auto"
-              src="assets/postman.png"
-              alt="postman icon"
-            />
-            <p className="my-4">Postman</p>
-          </div>
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded-lg"
+              data-aos="fade-up"
+              data-aos-delay={index * 100}
+              data-aos-duration={300}
+
+            >
+              <img className="w-20 mx-auto" src={skill.icon} alt={`${skill.name} icon`} />
+              <p className="my-4">{skill.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
