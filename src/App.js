05144a86f762ffcd03cd4cacce7716cb8aae0bcc
chathunipr <chathuninperera@gmail.com>
2024-07-02 23:25:01 +0530

    Updating the Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Hello from './Hello';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Qualification from './components/Qualification';
 
 function App() {
-  return (    
-      <div>
-        <Navbar/>
-        <Home/>
-        <About/>  
-        <Skills/>      
-        <Qualification/>      
-        <Projects/>
-        <Contact/>
-      </div>    
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
